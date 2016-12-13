b84ffb2b33db289d6e4da603f2e2d97890d1c3c8
Mitos Kalandiel <mitoskalandiel@gmail.com>
2016-12-13 18:16:55 +0000

    added JokerCrypto tests,lets see if it builds/releases.

---
+++ src/test/java/me/mitos_kalandiel/jokerlibs/JokerCryptoTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 smzb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.mitos_kalandiel.jokerlibs;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ *
+ * @author smzb
+ */
+public class JokerCryptoTest {
+    
+    public JokerCryptoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of generatePassword method, of class JokerCrypto.
+     */
+    @Test
+    public void testGeneratePassword() {
+        System.out.println("generatePassword");
+        int length = 5;
+        char[] result = JokerCrypto.generatePassword(length);
+        assertThat(result, is(not(0)));
+    }
+    
+}
